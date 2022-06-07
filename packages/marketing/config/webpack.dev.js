65e048691b1f8faf1ca19de74c001212129fb4a4
Cédric Lémery <cedric@lemery.dev>
2022-06-07 09:30:51 +0200

    section 5(2) : integration of the Container and Marketing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederatonPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -11,6 +12,13 @@ const devConfig = {
         }
     },
     plugins: [
+        new ModuleFederatonPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            }
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
         }),
